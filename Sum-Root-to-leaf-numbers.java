8c250d8ec43f03e90e0168ee88889d81a74da5b8
R. Mahadevan <97651598+maha201@users.noreply.github.com>
2023-08-16 21:37:10 +0530

    Sum-Root-to-leaf-numbers.java
    added sum root to leaf numbers

---
+++ Sum-Root-to-leaf-numbers.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<NodeSumPair> queue = new LinkedList<>();
+        queue.add(new NodeSumPair(root, 0));
+        int totalSum = 0;
+        while (!queue.isEmpty()) {
+            NodeSumPair removed = queue.remove();
+            TreeNode node = removed.node;
+            int currSum = removed.sum * 10 + node.val;
+            if (node.left == null && node.right == null) {
+                totalSum += currSum;
+            }
+            if (node.left != null) {
+                queue.add(new NodeSumPair(node.left, currSum));
+            }
+            if (node.right != null) {
+                queue.add(new NodeSumPair(node.right, currSum));
+            }
+        }
+        return totalSum;
+    }
+    
+    private static record NodeSumPair(TreeNode node, int sum) {}
+}
